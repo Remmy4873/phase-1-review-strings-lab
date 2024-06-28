0a2c8bba78618eee1fd58bc59fb45e8639a31a4d
Remmy4873 <Remmykiplangat4873@gmail.com>
2024-06-28 11:23:27 +0300

    add variable to index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code in this file!
+
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, ${Grace Hopper}!';
+
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK,  + GRACE HOPPER!';
+
+const shortGreeting = "Welcome, GRACE HOPPER!";
+
+
+
+
+
+
+ 
+
+
+
+
